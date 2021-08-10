Fixed uint8 overflow problem, which cause the input value unexpected

cim_model_3classes_BNN.py
@@ -89,9 +89,9 @@ class Classifier(nn.Module):
 		return x 
 
 def main():
-	train_set = DatasetFolder("./dataset/data_0711/lepton/train", loader=lambda x: Image.open(x), extensions="bmp", transform=train_tfm)
-	test_set = DatasetFolder("./dataset/data_0711/lepton/test", loader=lambda x: Image.open(x), extensions="bmp", transform=test_tfm)
-	val_set =  DatasetFolder("./dataset/data_0711/lepton/train", loader=lambda x: Image.open(x), extensions="bmp", transform=test_tfm)
+	train_set = DatasetFolder("./dataset/data_0711/grideye/train", loader=lambda x: Image.open(x), extensions="bmp", transform=train_tfm)
+	test_set = DatasetFolder("./dataset/data_0711/grideye/test", loader=lambda x: Image.open(x), extensions="bmp", transform=test_tfm)
+	val_set =  DatasetFolder("./dataset/data_0711/grideye/train", loader=lambda x: Image.open(x), extensions="bmp", transform=test_tfm)
 
 	train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 	test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)

simcim_model_8classes_BNN.py
@@ -1,8 +1,11 @@
 import torch
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt  
 import cv2, os, sys
 from torch.utils.data import Dataset
-from matplotlib import pyplot as plt
+
 from torch.utils.data import ConcatDataset, DataLoader, Subset
 import torch.nn as nn
 import torchvision.transforms as transforms
@@ -35,15 +38,15 @@ class Classifier(nn.Module):
 
 		self.cnn_layers1 = nn.Sequential(
 			
-			# CimSimConv2d(in_channels=1, out_channels=128, kernel_size=3, bias=False),
-            # nn.BatchNorm2d(128),
-            # nn.LeakyReLU(0.5),
-            # CimSimConv2d(in_channels=128, out_channels=64, kernel_size=3, bias=False),
-            # nn.BatchNorm2d(64),
-			# nn.LeakyReLU(0.5),
+			CimSimConv2d(in_channels=1, out_channels=128, kernel_size=7),
+            #nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.5),
+            CimSimConv2d(in_channels=128, out_channels=64, kernel_size=3),
+            #nn.BatchNorm2d(64),
+			nn.LeakyReLU(0.5),
 			
 			#input_size(1,30,40)
-			CimSimConv2d(1, 128, 3, 1), #output_size(16,66,66)
+			CimSimConv2d(64, 128, 3, 1), #output_size(16,66,66)
 			#nn.BatchNorm2d(128),
 			nn.LeakyReLU(0.5),
 			#nn.Dropout(0.2),
@@ -66,7 +69,7 @@ class Classifier(nn.Module):
 			#nn.Dropout(0.2),
 			nn.MaxPool2d(kernel_size = 2), #ouput_size(32,6,6)
 			#nn.LogSoftmax(),
-			BinarizeConv2d(32, 8, (3,2), 1) #ouput_size(4,2,3) without max :(32,24,34)
+			BinarizeConv2d(32, 8, (2,1), 1) #ouput_size(4,2,3) without max :(32,24,34)
 						
 		)
 		
@@ -112,10 +115,13 @@ def Load_data(path, cls):
 			tmp_input = cv2.imread(data_path + filename,cv2.IMREAD_UNCHANGED)
 			#print(tmp_input)
 			tmp_input = cv2.resize(tmp_input, (30,40), interpolation = cv2.INTER_AREA)
+			tmp_input = tmp_input.astype(int)
 			tmp_input = tmp_input//2
 			tmp_input = tmp_input - 63
+			tmp_input = np.where(tmp_input > 63, 63, tmp_input)
 			tmp_input = tmp_input[:, :, np.newaxis]
 			tmp_input = tmp_input.transpose(2,0,1)
+		
 			
 				
 			
@@ -142,7 +148,7 @@ def Load_data(path, cls):
 	label = np.array(label)
 	#print(data)
 	data = np.array(data)
-	print(label.shape)
+	#print(label.shape)
 	data = data.astype('float32')
 	
 	return data, label
@@ -151,138 +157,137 @@ def main():
 	# test_set = DatasetFolder("./dataset/8cls_srcnnimg/test", loader=lambda x: Image.open(x), extensions="jpg", transform=test_tfm)
 	# val_set =  DatasetFolder("./dataset/8cls_srcnnimg/train", loader=lambda x: Image.open(x), extensions="jpg", transform=test_tfm)
 	
+    train_data, train_label = Load_data("./dataset/8cls_grideye/train",8)
+    train = Cls_Dataset(train_data, train_label)
+    train_loader = DataLoader(
+            train, batch_size=100, shuffle=True,
+            num_workers=4, pin_memory=True, drop_last=True
+        )
+
+    test_data, test_label = Load_data("./dataset/8cls_grideye/test",8)
+
+    test = Cls_Dataset(test_data, test_label)
 	
-	train_data, train_label = Load_data("./dataset/8cls_grideye/train",8)
-	train = Cls_Dataset(train_data, train_label)
-	train_loader = DataLoader(
-	        train, batch_size=100, shuffle=True,
-	        num_workers=4, pin_memory=True, drop_last=True
-	    )
+    test_loader = DataLoader(
+            test, batch_size=100, shuffle=True,
+            num_workers=4, pin_memory=True, drop_last=True
+        )
 
-	test_data, test_label = Load_data("./dataset/8cls_grideye/test",8)
+    val_data, val_label = Load_data("./dataset/8cls_grideye/val",8)
 
-	test = Cls_Dataset(test_data, test_label)
+    val = Cls_Dataset(val_data, val_label)
 	
-	test_loader = DataLoader(
-	        test, batch_size=100, shuffle=True,
-	        num_workers=4, pin_memory=True, drop_last=True
-	    )
-
-	val_data, val_label = Load_data("./dataset/8cls_grideye/val",8)
-
-	val = Cls_Dataset(val_data, val_label)
-	
-	val_loader = DataLoader(
-	        val, batch_size=100, shuffle=True,
-	        num_workers=4, pin_memory=True, drop_last=True
-	    )
+    val_loader = DataLoader(
+            val, batch_size=100, shuffle=True,
+            num_workers=4, pin_memory=True, drop_last=True
+        )
 	
 	
 
 	
-	save_path = 'models.ckpt'
-	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-	model = Classifier().to(device)
-	optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-	criterion = nn.CrossEntropyLoss()
-	best_accuracy = 0.0
-	for epoch in range(num_epoch):
-		running_loss = 0.0
-		total = 0
-		correct = 0
-		for  i, data in enumerate(train_loader):
-			inputs, labels = data
-			inputs = inputs.to(device)
-			labels = labels.to(device)
-			optimizer.zero_grad()
+    save_path = 'models.ckpt'
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = Classifier().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    criterion = nn.CrossEntropyLoss()
+    best_accuracy = 0.0
+    for epoch in range(num_epoch):
+        running_loss = 0.0
+        total = 0
+        correct = 0
+        for  i, data in enumerate(train_loader):
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
 
-			outputs = model(inputs)
-			#print(outputs.shape)
+            outputs = model(inputs)
+            #print(outputs.shape)
 
-			loss = criterion(outputs, labels)
-			loss.backward()
-			#print(model)
-			#print(model.cnn_layers3[3].weight.grad)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            #print(model)
+            #print(model.cnn_layers3[3].weight.grad)
 
 
-			for p in list(model.parameters()):
-				if hasattr(p,'org'):
-					p.data.copy_(p.org)
+            for p in list(model.parameters()):
+                if hasattr(p,'org'):
+                    p.data.copy_(p.org) 
 
-			optimizer.step()
+            optimizer.step()
 
-			for p in list(model.parameters()):
-				if hasattr(p,'org'):
-					p.org.copy_(p.data.clamp_(-1,1))
-			
-			running_loss += loss.item()
-			total += labels.size(0)
-			_,predicted = torch.max(outputs.data,1)
-			#print(predicted)
-			#print("label",labels)
-			correct += (predicted == labels).sum().item()
-		train_acc = correct / total
+            for p in list(model.parameters()):
+                if hasattr(p,'org'):
+                    p.org.copy_(p.data.clamp_(-1,1))
+    		
+            running_loss += loss.item()
+            total += labels.size(0)
+            _,predicted = torch.max(outputs.data,1)
+            #print(predicted)
+            #print("label",labels)
+            correct += (predicted == labels).sum().item()
+        train_acc = correct / total
 
-		print(f"[ Train | {epoch + 1:03d}/{num_epoch:03d} ] loss = {running_loss:.5f}, acc = {train_acc:.5f}")
+        print(f"[ Train | {epoch + 1:03d}/{num_epoch:03d} ] loss = {running_loss:.5f}, acc = {train_acc:.5f}")
 		
-		model.eval()
-		with torch.no_grad():
-			correct = 0
-			total = 0
-			for  i, data in enumerate(val_loader):
-				inputs, labels = data
-				inputs = inputs.to(device)
-				labels = labels.to(device)
-				outputs = model(inputs)
-				_,predicted = torch.max(outputs.data,1)
-				total += labels.size(0)
-				correct += (predicted == labels).sum().item()
-			val_acc = correct / total
+        model.eval()
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for  i, data in enumerate(val_loader):
+                inputs, labels = data
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                outputs = model(inputs)
+                _,predicted = torch.max(outputs.data,1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+            val_acc = correct / total
 		
-		if val_acc > best_accuracy:
-			best_accuracy = val_acc
-			torch.save(model.state_dict(), save_path)
-			print("Save Model")
+        if val_acc > best_accuracy:
+            best_accuracy = val_acc
+            torch.save(model.state_dict(), save_path)
+            print("Save Model")
 
 
-		print(f"[ Val | {epoch + 1:03d}/{num_epoch:03d} ]  acc = {val_acc:.5f}")
+        print(f"[ Val | {epoch + 1:03d}/{num_epoch:03d} ]  acc = {val_acc:.5f}")
 
-	model = Classifier().to(device)
-	model.load_state_dict(torch.load(save_path))
-	model.eval()
-	stat = np.zeros((8,8))
-	with torch.no_grad():
-		correct = 0
-		total = 0
-		print(model)
-		for i, data in enumerate(test_loader):
-			inputs, labels = data
-			inputs = inputs.to(device)
-			labels = labels.to(device)
-			outputs = model(inputs)
-			#print(outputs.data)
-			_,predicted = torch.max(outputs.data,1)
-			#print(predicted)
-			total += labels.size(0)
-			correct += (predicted == labels).sum().item()
-			#print(labels)
-			#print(outputs.size())
+    model = Classifier().to(device)
+    model.load_state_dict(torch.load(save_path))
+    model.eval()
+    stat = np.zeros((8,8))
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        print(model)
+        for i, data in enumerate(test_loader):
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            #print(outputs.data)
+            _,predicted = torch.max(outputs.data,1)
+            #print(predicted)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            #print(labels)
+            #print(outputs.size())
 			
 
-			for k in range(len(predicted)):
-				if predicted[k] != labels[k]:
-					img = inputs[k].mul(255).byte()
-					img = img.cpu().numpy().squeeze(0)
-					img = np.moveaxis(img, 0, -1)
+            for k in range(len(predicted)):
+                if predicted[k] != labels[k]:
+                    img = inputs[k].mul(255).byte()
+                    img = img.cpu().numpy().squeeze(0)
+                    img = np.moveaxis(img, 0, -1)
 					
-					predict = predicted[k].cpu().numpy()
-					label = labels[k].cpu().numpy()
-					path = "test_result/predict:"+str(predict)+"_labels:"+str(label)+".jpg"
-					stat[int(label)][int(predict)] += 1
-		ax = sns.heatmap(stat, linewidth=0.5)
-		plt.xlabel('Prediction')
-		plt.ylabel('Label')
-		plt.savefig('heatmap.jpg')
-		print('Test Accuracy:{} %'.format((correct / total) * 100))
+                    predict = predicted[k].cpu().numpy()
+                    label = labels[k].cpu().numpy()
+                    path = "test_result/predict:"+str(predict)+"_labels:"+str(label)+".jpg"
+                    stat[int(label)][int(predict)] += 1
+        ax = sns.heatmap(stat, linewidth=0.5)
+        plt.xlabel('Prediction')
+        plt.ylabel('Label')
+        plt.savefig('heatmap.jpg')
+        print('Test Accuracy:{} %'.format((correct / total) * 100))
 if __name__ == '__main__':
-	main()
+    main()